f1e1bfb48c5c5b6b987c17172afec59826ed3401
DilanKz <Dilanmallawarachchi555@gmail.com>
2023-10-09 20:34:14 +0530

    added order and placeorder controllers

---
+++ BackEnd/src/main/java/lk/ijse/spring/repo/OrdersRepo.java
@@ -0,0 +1,12 @@
+package lk.ijse.spring.repo;
+
+import lk.ijse.spring.entity.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * `@authority Tharindu Dilan`
+ * 8:30 PM
+ * 2023-10-09 - 10 - 2023
+ */
+public interface OrdersRepo extends JpaRepository<Item,String> {
+}
---
+++ BackEnd/src/main/java/lk/ijse/spring/repo/PlaceOrderRepo.java
@@ -0,0 +1,13 @@
+package lk.ijse.spring.repo;
+
+import lk.ijse.spring.entity.Item;
+import lk.ijse.spring.entity.Orders;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * `@authority Tharindu Dilan`
+ * 8:30 PM
+ * 2023-10-09 - 10 - 2023
+ */
+public interface PlaceOrderRepo extends JpaRepository<Orders,String> {
+}
